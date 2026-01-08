96ca027f0254e142bd13fd757dc4969c55529032
johnchoi <jchoi@schellgames.com>
2026-01-08 16:22:22 -0500

    test again, variant launch true

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export const config = {
   // Enable Variant Launch for iOS support
   // Set to false to save your 3000 free monthly launches
   // When disabled: Only Android WebXR will work, iOS will not be supported
-  enableVariantLaunch: false,
+  enableVariantLaunch: true,
   
   // Variant Launch SDK Key (only used if enableVariantLaunch is true)
   variantLaunchKey: 'QThcf5w5xXfLziijsLzTXBbQIqLomAB8',
